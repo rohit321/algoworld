2b4ee201fca304d9f369883df71a800a882cf6e4
rohit321 <rohit.pegallapati@gmail.com>
2014-09-07 22:24:12 -0700

    Update AllUnique.java

--- cci/AllUnique.java
+++ cci/AllUnique.java
@@ -1,1 +1,44 @@
-//hello
+/**
+*Program to check if all the chars of a string are unique
+* using extra data structures is allowed
+* /
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class AllUnique {
+	
+	private static Scanner sc;
+	
+	public static boolean isAllUnique(String input)
+	{
+		char arr[] = input.toCharArray();
+		
+		Set<Character> charSet = new HashSet<Character>();
+		
+		for(int i=0; i < arr.length ; i++ )
+		{
+			charSet.add(arr[i]);
+		}
+		
+		if(arr.length > charSet.size())
+			return false;
+		
+		return true;
+	}
+	
+	public static void main(String args[])
+	{
+		System.out.println("Enter string");
+		sc = new Scanner(System.in);
+		String input = sc.next();
+		
+		String result = isAllUnique(input) ? "all chars are unique" : "repeated chars present";
+		
+		System.out.println(result);
+	}
+
+}
+
+
